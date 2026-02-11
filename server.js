f2bc1525253515e126f3db2675d2c55927a2590a
abshah2005 <abdullah03350904415@gmail.com>
2026-02-12 01:17:21 +0500

    fixes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello from Express server!');
 });
 
+app.get("/testing",(req,res)=>{
+    res.send("hi this is abdullah");
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
